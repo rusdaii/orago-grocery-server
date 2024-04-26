5d5889b15d97145e036f033fb93c1375ba1fbfea
rusdaii <rusdaii.html@gmail.com>
2024-04-27 06:47:23 +0700

    add server configuration & populate profile response google auth

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,27 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  // bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    await strapi
+      .service("plugin::users-permissions.providers-registry")
+      .register(`google`, ({ purest }) => async ({ query }) => {
+        const google = purest({ provider: "google" });
+
+        const res = await google
+          .get("https://www.googleapis.com/oauth2/v3/userinfo")
+          .auth(query.access_token)
+          .request();
+
+        const { body } = res;
+
+        return {
+          username: body.email.split("@")[0],
+          fullName: body.email.split("@")[0],
+          email: body.email,
+          image: body.picture,
+          provider: "google",
+        };
+      });
+  },
 };
