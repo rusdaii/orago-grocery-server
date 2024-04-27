20595cc0e18b72064b1eafd1c74c7f5e877e4a26
rusdaii <rusdaii.html@gmail.com>
2024-04-27 07:02:36 +0700

    fix: change server config

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("", "https://orago-grocery-server.onrender.com"),
+  proxy: true,
+  url: env("", "http://localhost:1337"),
   app: {
     keys: env.array("APP_KEYS"),
   },
