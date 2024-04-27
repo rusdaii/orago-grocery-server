43ea01b79cb38f974000f81f094d15497354e765
rusdaii <rusdaii.html@gmail.com>
2024-04-27 07:09:35 +0700

    fix: change server url to live url

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   proxy: true,
-  url: env("", "http://localhost:1337"),
+  url: env("", "https://orago-grocery-server.onrender.com/"),
   app: {
     keys: env.array("APP_KEYS"),
   },
